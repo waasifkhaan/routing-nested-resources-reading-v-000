a7e781550e090aceaae4c86edd92b2729aebc2ad
Wasif Khan <waasif.khaan@gmail.com>
2018-10-15 23:37:16 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  # 
+  #
   # resources :authors, only: [:show]
-  # 
+  #
   # resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
   root 'posts#index'
